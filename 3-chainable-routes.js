014a3bad53d184d2c19d26b0261de83a76904ea4
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-03-29 19:27:24 +0200

    chainable route methods

---
+++ 3-chainable-routes.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+app 
+    .route('/home')
+    .get((request, response, next) => {
+        response.type('text/html')
+        response.write('<!DOCTYPE html>')
+        next()
+    })
+    .get((request, response, next) => {
+        response.end(`
+        <html lang='en'> 
+            <head> 
+            <meta charset='utf-8'> 
+            <title>WebApp powered by ExpressJS</title> 
+            </head> 
+            <body role='application'> 
+                <form method='post' action='/home'> 
+                    <input type='text' /> 
+                    <button type='submit'>Send</button> 
+                </form> 
+            </body> 
+        </html> 
+        `)
+    })
+    .post((request, response, next) => {
+        response.send('Got it!')
+    })
+
+    
+app.listen(
+    1337,
+    () => console.log('Web Server running on port 1337')
+)
