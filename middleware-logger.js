65374fa9748cdf14338d7934700f74b81d943f2b
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-03-29 21:13:57 +0200

    writing configurable middleware functions

---
+++ middleware-logger.js
@@ -0,0 +1,15 @@
+// configurable logger middleware function
+
+const logger = (options) => (request, response, next) => {
+    if (typeof options === 'object'
+        && options !== null 
+        && options.enable) {
+            console.log(
+                'Status Code:', response.statusCode,
+                'URL:', request.originalUrl,
+            )
+        }
+    next()
+}
+
+module.exports = logger
