150faf2ce1d809ec356ebea4298c9d5900c2086b
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-03-29 23:01:52 +0200

    managing and creating virtual subdomains

---
+++ user-subdomains.js
@@ -0,0 +1,24 @@
+const express = require('express') 
+const vhost = require('vhost') 
+const app = express()
+
+const users = express.Router()
+users.get('/', (request, response, next) =>{
+    const username = request 
+        .vhost[0]
+        .split('-')
+        .map(name => (
+            name[0].toUpperCase() +
+            name.slice(1)
+        ))
+        .join(' ')
+    response.send(`Hello, ${username}`)
+})
+
+// mount the router
+app.use(vhost('*.localhost', users))
+
+app.listen( 
+    1337, 
+   () => console.log('Web Server running on port 1337'), 
+)
